d15381b6cb89f09e9b7dfe8408cf6e2c4abd9d04
Scott Forber <scottforber@gmail.com>
2020-07-23 18:14:37 +0100

    Add caesar-cipher.rb

---
+++ caesar-cipher.rb
@@ -0,0 +1,44 @@
+STDOUT.sync = true
+
+def createAlphabet()
+    alphabet = ("a".."z").to_a
+    character_hash = Hash.new
+    i = 0
+    until i == 26 do
+        character_hash[alphabet[i]] = i
+        i += 1
+    end
+    return character_hash
+end
+
+def caesarCipher(message, shift)
+    character_hash = createAlphabet()
+    encryptedMessage = []
+    message.chomp!
+    message = message.split("")
+    for char in message
+        if char != " "
+            if char == char.upcase
+                char.downcase!
+                charShift = (character_hash[char] - shift) % 26
+                charShift = character_hash.key(charShift)
+                encryptedMessage.push(charShift.upcase)
+            else
+                charShift = (character_hash[char] - shift) % 26
+                encryptedMessage.push(character_hash.key(charShift))
+            end
+        else
+            encryptedMessage.push(char)            
+        end
+    end
+    puts encryptedMessage.join("")
+end
+
+puts ("Enter a message: ")
+message = gets.chomp
+puts ("Enter the shift amount: ")
+shift = gets.chomp.to_i
+
+caesarCipher(message, shift)
+
+
